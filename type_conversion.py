7f5eb866f73c8b31fedc1d69e8fcbadf2eca2324
FreedomWriter <natalienetworks@gmail.com>
2020-06-30 10:58:59 -0500

    type_conversion

---
+++ type_conversion.py
@@ -0,0 +1,11 @@
+# TYPE CONVERSION
+
+birth_year = input("What year were you born? ")
+print(f'Your age is {2020 - int(birth_year)}.')
+current_year = 2020
+print(f'\nYour age is {current_year - int(birth_year)}.')
+age = 2020 - int(birth_year) 
+print(f'Your age is {age}.')
+
+
+print('\n############################################################################################\n')
