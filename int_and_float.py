b715e7f2b6188533168b0f4d9a30bc1b09e49618
FreedomWriter <natalienetworks@gmail.com>
2020-06-30 10:31:18 -0500

    int_and_float

---
+++ int_and_float.py
@@ -0,0 +1,11 @@
+# int and float
+# Float takes up more space in memory than int - actions will convert int to float when necessary, but will not convert float to int
+print("\n\n#DATA TYPES\n\n# # int and float\n# #  # Float takes up more space in memory than int - actions will convert int to float when necessary, but will not convert float to int")
+
+print(f'\ntype(2 + 4)   \n# prints: {type(2 + 4)}')
+
+print(f'\ntype(2 - 4)   \n# prints: {type(2 - 4)}')
+
+print(f'\ntype(2 * 4)   \n# prints: {type(2 * 4)}')
+
+print(f'\ntype(2 / 4)   \n# prints: {type(2 / 4)}')
