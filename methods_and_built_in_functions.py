3bee50d06f36f908c03df1e6ab9597f74eac38f5
FreedomWriter <natalienetworks@gmail.com>
2020-06-30 11:00:10 -0500

    methods and built in functions

---
+++ methods_and_built_in_functions.py
@@ -0,0 +1,28 @@
+# Methods
+  # methods are invoked by dot notation, keyword, ()
+  # .format()
+  # on the class they belong to
+
+quote = "to be or not to be"
+
+print(f'quote = "to be or not to be"\n')
+
+print(f'print(quote.upper()):\n  # prints {quote.upper()}\n')
+
+print(f'print(quote.find("be")):\n  # prints "{quote.find("be")}", which is the position in which it finds the index in which begins the first occurance \n')
+
+# print(f'print(quote.replace("be", "me")):\n \n # prints "{quote.replace("be", "me")}":# repalcing all occurances of the first value with the second value\n # not permananent - strings are immutable\n # .replace() actually creates a new value wich can be assigned to mememory\n')
+
+print(f'print(quote.capitalize()):\n  # prints "{quote.capitalize()}"\n')
+
+
+print('\n############################################################################################\n')
+
+
+# BOOLEANS - note capital T & F
+
+print(f'bool(1) evaluates to {bool(1)}')
+print(f'bool(0) evaluates to {bool(0)}\n')
+
+
+print('\n############################################################################################\n')
