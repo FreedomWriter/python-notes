3c0d3a5264a70ff60165cde4f6634b7c4b9fce07
FreedomWriter <natalienetworks@gmail.com>
2020-06-30 10:33:19 -0500

    math

---
+++ math.py
@@ -0,0 +1,56 @@
+# Operator to the left of the equal sign will take prev value and perform given math operation
+print("# Operator to the LEFT of the equal sign will take prev value and perform given math operation")
+
+some_value = 5
+some_value +=  2
+some_value_divided = 5
+some_value_divided /=  2
+
+print(f'\nsome_value = 5   \n# prints: {5}')
+
+print(f'\nsome_value +=  2   \n# prints: {some_value}')
+print(f'\nsome_value /=  2   \n# prints: {some_value_divided}')
+
+
+print('\n############################################################################################\n')
+
+# WHAT ARE THESE EVEN CALLED?
+# # `**` means to the power of
+# `//` will round down to the nearest int
+# `%` (modulo) used to represent remainder
+print("# WHAT ARE THESE EVEN CALLED?")
+
+print("# `**` means to the power of")
+print("# `//` will round down to the nearest int")
+print("# `%` (modulo) used to represent remainder")
+
+print(f'\n2 ** 3 \n  # prints: {2 ** 3}')
+print(f'\n3 // 4   \n# prints: {3 // 4}')
+print(f'\n5 // 4   \n# prints: {5 // 4}')
+print(f'\n5 % 4   \n# prints: {5 % 4}')
+print(f'\n6 % 4   \n# prints: {6 % 4}')
+
+
+print('\n############################################################################################\n')
+
+# MATH FUNCTIONS
+# print(round(3.4)) # evalates to 2 - 3.6 would evaluate to 4
+# print(abs(-20)) # evalues to `20` (the absolute value)
+
+print('# MATH FUNCTIONS')
+print(f'\nround(3.4)   \n# prints: {round(3.4)}')
+print(f'\nround(3.5)   \n# prints: {round(3.5)}')
+print(f'\nabs(-20)   \n# prints: {abs(-20)}\n# which is the absolute value of -20')
+
+print('\n############################################################################################\n')
+
+
+#Operator Precedence - order of operations (PEMDAS)
+# ()
+# **
+# * /
+# + -
+print(f'# OPERATOR PRECEDENCE - order of operations (PEMDAS)\n. 1. ()\n. 2. **\n. 3. * /\n. 4. + -'
+)
+
+print('\n############################################################################################\n')
