906bf76f6acb2a561864280f5d7c163eb1cea53d
FreedomWriter <natalienetworks@gmail.com>
2020-08-03 19:17:24 -0500

    binary search

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -4,7 +4,8 @@ import time
 my_range = 100
 my_size = 15
 
-random_nums = random.sample(range(my_range), my_size)
+# random_nums = random.sample(range(my_range), my_size)
+random_nums = [4, 10, 13, 17, 21, 26, 29, 31, 32, 50, 67, 60, 74, 85, 92]
 
 print(random_nums)
 num_to_find = 12
@@ -17,12 +18,29 @@ def linear_search(arr, target):
         
     return False
 
-print(linear_search(random_nums, num_to_find))
+# print(linear_search(random_nums, num_to_find))
 
-random_nums.sort()
-print(random_nums)
+# random_nums.sort()
+# print(random_nums)
 
 def binary_search(arr, target):
-    # find middle index of array
-    # compare the value in the middle with target
-    #
+    start = 0
+    end = (len(arr) -1)
+
+    found = False
+    while end >= start and not found:
+        
+        # find middle index of array
+        middle_index = (start + end) //2
+        # compare the value in the middle with target# if the middle value is the same as target, set found to True
+        if arr[middle_index] == target:
+            found = True
+        # move start or end index closer to one another, and shrink our search space
+        else:
+            if target < arr[middle_index]:
+                end = (middle_index -1) 
+            if target > arr[middle_index]:
+                start = (middle_index + 1)
+    return found
+    
+print(binary_search(random_nums, 13))
